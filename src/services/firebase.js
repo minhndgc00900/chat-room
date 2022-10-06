221484410c02e8be6045c62bee4a23ce694ee414
Minh Nguyen <minhnd0496@gmail.com>
2022-10-06 17:59:35 +0700

    add variable instead of value

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,13 +8,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-nXmPxQdpuN9Z0pIOWB1sacf6hwNZg7w",
-  authDomain: "chat-room-98748.firebaseapp.com",
-  projectId: "chat-room-98748",
-  storageBucket: "chat-room-98748.appspot.com",
-  messagingSenderId: "370097747557",
-  appId: "1:370097747557:web:25b6f19f8f60acf660b88c",
-  measurementId: "G-F16WELFC8Q"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_PACKAGE,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
