b8ff54ba3c798fc2d916ae4d6d42fb60280ef3ef
Minh Nguyen <minhnd0496@gmail.com>
2022-10-06 18:21:20 +0700

    save token on local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import './App.css';
-import { AuthenticatedApp } from './components/AuthenticatedApp';
-import { UnauthenticatedApp } from './components/UnauthenticatedApp';
-import { useAuth } from './hooks/useAuth';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { AuthenticatedApp } from "./components/AuthenticatedApp";
+import { UnauthenticatedApp } from "./components/UnauthenticatedApp";
+import { useAuth } from "./hooks/useAuth";
 
 const App = () => {
   const { user, login } = useAuth();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const accessToken = localStorage.getItem("accessToken");
+
+  useEffect(() => {
+    if (user || accessToken) {
+      setIsAuthenticated(true);
+    }
+  }, [accessToken, user])
+  
+
   return (
     <div className="container">
       <h1>Chat Room</h1>
-      {!user && <button className="btn-signin" onClick={() => login()}> Sign In With Google</button>}
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      {isAuthenticated ? (
+        <AuthenticatedApp />
+      ) : (
+        <>
+          <button className="btn-signin" onClick={() => login()}>
+            {" "}
+            Sign In With Google
+          </button>
+          <UnauthenticatedApp />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/context/auth.js
+++ src/context/auth.js
@@ -8,7 +8,11 @@ const AuthProvider = (props) => {
 
     const login = async () => {
         const user = await loginWithGoogle();
-        console.log(222, user);
+        console.log(23, user);
+        localStorage.setItem("name", user.displayName);
+        localStorage.setItem("email", user.email);
+        localStorage.setItem("profilePic", user.profilePic);
+        localStorage.setItem("accessToken", user.accessToken);
         if (!user) {
             // TODO: Handle failed login
         }
